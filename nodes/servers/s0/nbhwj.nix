51e3e6d4fa596977117667c4a6ac3b2746bd3fbf
Glen Huang <me@glenhuang.com>
2025-03-14 12:39:57 +0800

    s0: add hsts header to nbhwj

--- nodes/servers/s0/nbhwj.nix
+++ nodes/servers/s0/nbhwj.nix
@@ -47,44 +47,44 @@ in
   systemd.tmpfiles.rules = [
     "d /srv/www/wordpress/nbhwj - ${user} ${config.users.users.${user}.group}"
   ];
-  services.caddy = {
-    enable = true;
-    virtualHosts = {
-      ${domain} = {
-        useACMEHost = domain;
-        extraConfig = ''
-          header -Server
-          root /srv/www/wordpress/nbhwj
-          file_server
-          encode gzip zstd
+  services.caddy.virtualHosts = {
+    ${domain} = {
+      useACMEHost = domain;
+      extraConfig = ''
+        header {
+          -Server
+          Strict-Transport-Security max-age=63072000
+        }
+        root /srv/www/wordpress/nbhwj
+        file_server
+        encode gzip zstd
 
-          @cache {
-            not header_regexp Cookie "comment_author|wordpress_[a-f0-9]+|wp-postpass|wordpress_logged_in"
-            not path_regexp "(/wp-admin/|/xmlrpc.php|/wp-(app|cron|login|register|mail).php|wp-.*.php|/feed/|index.php|wp-comments-popup.php|wp-links-opml.php|wp-locations.php|sitemap(index)?.xml|[a-z0-9-]+-sitemap([0-9]+)?.xml)"
-            not method POST
-            not expression {query} != '''
-          }
-          route @cache {
-            try_files /wp-content/cache/supercache/{host}{uri}/index-https.html /wp-content/cache/supercache/{host}{uri}/index.html {path} {path}/index.php?{query}
-          }
+        @cache {
+          not header_regexp Cookie "comment_author|wordpress_[a-f0-9]+|wp-postpass|wordpress_logged_in"
+          not path_regexp "(/wp-admin/|/xmlrpc.php|/wp-(app|cron|login|register|mail).php|wp-.*.php|/feed/|index.php|wp-comments-popup.php|wp-links-opml.php|wp-locations.php|sitemap(index)?.xml|[a-z0-9-]+-sitemap([0-9]+)?.xml)"
+          not method POST
+          not expression {query} != '''
+        }
+        route @cache {
+          try_files /wp-content/cache/supercache/{host}{uri}/index-https.html /wp-content/cache/supercache/{host}{uri}/index.html {path} {path}/index.php?{query}
+        }
 
-          @disallowed {
-            path /xmlrpc.php
-            path *.sql
-            path /wp-content/uploads/*.php
-          }
-          rewrite @disallowed /
+        @disallowed {
+          path /xmlrpc.php
+          path *.sql
+          path /wp-content/uploads/*.php
+        }
+        rewrite @disallowed /
 
-          php_fastcgi unix/${config.services.phpfpm.pools.wordpress-nbhwj.socket}
-        '';
-      };
-      "www.${domain}" = {
-        useACMEHost = "www.${domain}";
-        extraConfig = ''
-          header -Server
-          redir https://${domain}{uri} 301
-        '';
-      };
+        php_fastcgi unix/${config.services.phpfpm.pools.wordpress-nbhwj.socket}
+      '';
+    };
+    "www.${domain}" = {
+      useACMEHost = "www.${domain}";
+      extraConfig = ''
+        header -Server
+        redir https://${domain}{uri} 301
+      '';
     };
   };
   services.redis.servers.wordpress-nbhwj = {
