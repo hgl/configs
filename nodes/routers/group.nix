050b4b72a294351013240d57a6d0a64cc425037a
Glen Huang <me@glenhuang.com>
2025-07-27 22:03:23 +0800

    routers: remove unused meta config

--- nodes/routers/group.nix
+++ nodes/routers/group.nix
@@ -1,73 +1,11 @@
 {
   common =
-    {
-      common,
-      nodes,
-      lib',
-    }:
+    { lib, nodes, ... }:
     {
       os = "nixos";
       channel = "unstable";
-      subrouter = false;
-      # Used by the mobileconfig script
-      fqdn = nodes.current.config.networking.fqdn;
-      dhcp = {
-        pool = {
-          start = 100;
-          end = 250;
-        };
-      };
       deploy = {
-        targetHost = "root@${nodes.current.config.networking.fqdn}";
+        targetHost = lib.mkDefault "root@${nodes.current.config.networking.fqdn}";
       };
-    };
-  r0 =
-    { nodes, lib' }:
-    {
-      index = 0;
-      interfaces = lib'.interfaces nodes.current [
-        {
-          name = "lan";
-          ports = [
-            "enp1s0"
-            "enp2s0"
-            "enp3s0"
-          ];
-        }
-        {
-          name = "guest-lan";
-          vlan = true;
-        }
-        { name = "y"; }
-        { name = "tailscale"; }
-        { name = "ipsec"; }
-        { name = "wireguard"; }
-        { name = "guest-ipsec"; }
-      ];
-    };
-  r1 =
-    { nodes, lib' }:
-    {
-      subrouter = true;
-      index = 1;
-      interfaces = lib'.interfaces nodes.current [
-        {
-          name = "lan";
-          ports = [
-            "enp1s0"
-            "enp2s0"
-            "eno1"
-          ];
-        }
-        {
-          name = "guest-lan";
-          vlan = true;
-        }
-        { name = "y"; }
-        { name = "tailscale"; }
-        { name = "ipsec"; }
-        { name = "wireguard"; }
-        { name = "guest-ipsec"; }
-      ];
     };
 }
