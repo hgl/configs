5d7ff54e92f2295b0a792a23873188e485095fba
Glen Huang <me@glenhuang.com>
2025-08-16 21:01:21 +0800

    routers: fix tailscale mss clamping

--- nodes/routers/common/tailscale.nix
+++ nodes/routers/common/tailscale.nix
@@ -52,7 +52,7 @@
     family = "inet";
     content = ''
       chain forward {
-        type filter hook forward priority filter;
+        type filter hook forward priority mangle;
         oifname "${config.services.tailscale.interfaceName}" tcp flags syn tcp option maxseg size set rt mtu
       }
     '';
