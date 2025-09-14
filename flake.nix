16ff6dfa21cef54a52f9b2aa8ab1f22fd74f01a9
Glen Huang <me@glenhuang.com>
2025-09-14 22:29:14 +0800

    remove unused flake inputs

--- flake.nix
+++ flake.nix
@@ -2,9 +2,6 @@
   inputs = {
     self.submodules = true;
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable-nixos.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-stable-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
-    determinate-unstable.url = "github:DeterminateSystems/determinate";
     nix-darwin-unstable = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
@@ -24,10 +21,6 @@
     nix-networkd-unstable = {
       url = "github:hgl/nix-networkd";
       # url = "/Users/hgl/dev/nix-networkd";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-    virby-unstable-darwin = {
-      url = "github:quinneden/virby-nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     nixverse = {
