7eaa197a18aa23a324acaa731694c74f7d24c131
Glen Huang <me@glenhuang.com>
2025-12-28 14:46:31 +0800

    routers: use ddclient

--- nodes/routers/common/ddns.nix
+++ nodes/routers/common/ddns.nix
@@ -3,18 +3,16 @@
   ...
 }:
 {
-  sops.secrets = {
-    ddnsCloudflareZoneId = { };
-    ddnsCloudflareToken = { };
+  sops.secrets.ddnsCloudflareToken = { };
+
+  services.ddclient = {
+    enable = true;
+    interval = "5min";
+    usev4 = "ifv4, ifv4=${config.networkd.interfaces.wan.name}";
+    usev6 = "ifv6, ifv6=${config.networkd.interfaces.wan.name}";
+    protocol = "cloudflare";
+    domains = [ config.networking.fqdn ];
+    zone = config.networking.domain;
+    passwordFile = config.sops.secrets.ddnsCloudflareToken.path;
   };
-  networkd.interfaces.wan.ddns = [
-    {
-      domains = [ config.networking.fqdn ];
-      provider = {
-        type = "cloudflare";
-        zoneIdFile = config.sops.secrets.ddnsCloudflareZoneId.path;
-        apiTokenFile = config.sops.secrets.ddnsCloudflareToken.path;
-      };
-    }
-  ];
 }
