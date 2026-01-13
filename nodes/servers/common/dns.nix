85ff6ae2285f762193f3f851726f7c5469f55169
Glen Huang <me@glenhuang.com>
2026-01-13 16:25:46 +0000

    servers: remove doh server

--- nodes/servers/common/dns.nix
+++ nodes/servers/common/dns.nix
@@ -12,28 +12,28 @@
     "2606:4700:4700::1001"
   ];
 
-  services.doh-server = {
-    enable = true;
-    settings = {
-      listen = [
-        "[::1]:8053"
-      ];
-      upstream = map (
-        address:
-        "udp:${
-          lib'.addressPortString {
-            inherit address;
-            port = 53;
-          }
-        }"
-      ) config.networking.nameservers;
-    };
-  };
+  # services.doh-server = {
+  #   enable = true;
+  #   settings = {
+  #     listen = [
+  #       "[::1]:8053"
+  #     ];
+  #     upstream = map (
+  #       address:
+  #       "udp:${
+  #         lib'.addressPortString {
+  #           inherit address;
+  #           port = 53;
+  #         }
+  #       }"
+  #     ) config.networking.nameservers;
+  #   };
+  # };
 
-  services.nginx = {
-    virtualHosts.host.locations."/dns-query" = {
-      proxyPass = "http://${lib.elemAt config.services.doh-server.settings.listen 0}/dns-query";
-      recommendedProxySettings = true;
-    };
-  };
+  # services.nginx = {
+  #   virtualHosts.host.locations."/dns-query" = {
+  #     proxyPass = "http://${lib.head config.services.doh-server.settings.listen}/dns-query";
+  #     recommendedProxySettings = true;
+  #   };
+  # };
 }
