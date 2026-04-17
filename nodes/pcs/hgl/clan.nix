01f0ad6a375fd7a7596d0f74cfbf62d529a9428e
Glen Huang <me@glenhuang.com>
2026-04-17 12:30:08 +0800

    hgl: add clan substituter

---
+++ nodes/pcs/hgl/clan.nix
@@ -0,0 +1,9 @@
+{
+  nix.settings = {
+    extra-substituters = [ "https://cache.clan.lol" ];
+    extra-trusted-public-keys = [
+      "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+}
