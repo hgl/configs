6c742392e3cf09721b1f8a3f5f312858a434c0a8
Glen Huang <me@glenhuang.com>
2026-06-30 14:18:36 +0800

    fish: add zmx to prompt

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -4,7 +4,11 @@
     enable = true;
     functions = {
       fish_prompt = ''
-        ${if nodes.current.groups ? pcs then "printf '❯ '" else "printf '%s❯ ' (prompt_hostname)"}
+        printf '%s' (prompt_hostname)
+        if test -n "$ZMX_SESSION"
+          printf '[%s]' "$ZMX_SESSION"
+        end
+        printf '❯ '
       '';
       fish_right_prompt = ''
         echo -n -s (prompt_pwd --full-length-dirs 2) (fish_vcs_prompt) ' ' (date +%H:%M:%S)
