4f2c39623c010f22d64d420210a421f807557cf0
Glen Huang <me@glenhuang.com>
2026-09-22 17:42:52 +0800

    glen: udpate ssh config

--- nodes/pcs/glen/users/hgl/home.nix
+++ nodes/pcs/glen/users/hgl/home.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   pkgs,
   pkgs',
@@ -20,12 +21,29 @@
 
   programs.ssh = {
     settings = {
+      # Inside an ssh login, use the forwarded agent. ssh keeps the first
+      # IdentityAgent it sees, so order this before every other block ("*" is
+      # always last).
+      forwarded-agent =
+        lib.hm.dag.entryBefore
+          (lib.attrNames (
+            lib.removeAttrs config.programs.ssh.settings [
+              "forwarded-agent"
+              "*"
+            ]
+          ))
+          {
+            header = ''Match exec "printenv SSH_CONNECTION"'';
+            IdentityAgent = "SSH_AUTH_SOCK";
+          };
       "github-hgl" = {
         HostName = "github.com";
         User = "git";
         IdentityFile = "~/.ssh/id_hgl.pub";
-        IdentityAgent = lib.toJSON "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
         IdentitiesOnly = true;
+      };
+      "*" = {
+        IdentityAgent = lib.toJSON "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
       };
     };
   };
